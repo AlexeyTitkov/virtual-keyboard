e982ed717130d3a6855af5024c71ee660e494c07
AlexeyTitikov <alexey.titkov.work@gmail.com>
2023-05-01 22:54:54 +0300

    feat: add gh-pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,189 @@
-import { keyboardKeysEn } from 'https://alexeytitkov.github.io/virtual-keyboard/src/js//_keyboard-keys-en.js';
-import { keyboardKeysRu } from 'https://alexeytitkov.github.io/virtual-keyboard/src/js//_keyboard-keys-ru.js';
-import { Button } from 'https://alexeytitkov.github.io/virtual-keyboard/src/js//_button.js';
+const keyboardKeysEn = [
+  { key: '`', shiftKey: '~', code: 'Backquote' },
+  { key: '1', shiftKey: '!', code: 'Digit1' },
+  { key: '2', shiftKey: '@', code: 'Digit2' },
+  { key: '3', shiftKey: '#', code: 'Digit3' },
+  { key: '4', shiftKey: '$', code: 'Digit4' },
+  { key: '5', shiftKey: '%', code: 'Digit5' },
+  { key: '6', shiftKey: '^', code: 'Digit6' },
+  { key: '7', shiftKey: '&', code: 'Digit7' },
+  { key: '8', shiftKey: '*', code: 'Digit8' },
+  { key: '9', shiftKey: '(', code: 'Digit9' },
+  { key: '0', shiftKey: ')', code: 'Digit0' },
+  { key: '-', shiftKey: '_', code: 'Minus' },
+  { key: '=', shiftKey: '+', code: 'Equal' },
+  { key: 'Backspace', shiftKey: 'Backspace', code: 'Backspace' },
+  { key: 'Tab', shiftKey: 'Tab', code: 'Tab' },
+  { key: 'q', shiftKey: 'Q', code: 'KeyQ' },
+  { key: 'w', shiftKey: 'W', code: 'KeyW' },
+  { key: 'e', shiftKey: 'E', code: 'KeyE' },
+  { key: 'r', shiftKey: 'R', code: 'KeyR' },
+  { key: 't', shiftKey: 'T', code: 'KeyT' },
+  { key: 'y', shiftKey: 'Y', code: 'KeyY' },
+  { key: 'u', shiftKey: 'U', code: 'KeyU' },
+  { key: 'i', shiftKey: 'I', code: 'KeyI' },
+  { key: 'o', shiftKey: 'O', code: 'KeyO' },
+  { key: 'p', shiftKey: 'P', code: 'KeyP' },
+  { key: '[', shiftKey: '{', code: 'BracketLeft' },
+  { key: ']', shiftKey: '}', code: 'BracketRight' },
+  { key: '\\', shiftKey: '|', code: 'Backslash' },
+  { key: 'Del', shiftKey: 'Del', code: 'Delete' },
+  { key: 'Caps Lock', shiftKey: 'Caps Lock', code: 'CapsLock' },
+  { key: 'a', shiftKey: 'A', code: 'KeyA' },
+  { key: 's', shiftKey: 'S', code: 'KeyS' },
+  { key: 'd', shiftKey: 'D', code: 'KeyD' },
+  { key: 'f', shiftKey: 'F', code: 'KeyF' },
+  { key: 'g', shiftKey: 'G', code: 'KeyG' },
+  { key: 'h', shiftKey: 'H', code: 'KeyH' },
+  { key: 'j', shiftKey: 'J', code: 'KeyJ' },
+  { key: 'k', shiftKey: 'K', code: 'KeyK' },
+  { key: 'l', shiftKey: 'L', code: 'KeyL' },
+  { key: ';', shiftKey: ':', code: 'Semicolon' },
+  { key: "'", shiftKey: '"', code: 'Quote' },
+  { key: 'Enter', shiftKey: 'Enter', code: 'Enter' },
+  { key: 'Shift', shiftKey: 'Shift', code: 'ShiftLeft' },
+  { key: 'z', shiftKey: 'Z', code: 'KeyZ' },
+  { key: 'x', shiftKey: 'X', code: 'KeyX' },
+  { key: 'c', shiftKey: 'C', code: 'KeyC' },
+  { key: 'v', shiftKey: 'V', code: 'KeyV' },
+  { key: 'b', shiftKey: 'B', code: 'KeyB' },
+  { key: 'n', shiftKey: 'N', code: 'KeyN' },
+  { key: 'm', shiftKey: 'M', code: 'KeyM' },
+  { key: ',', shiftKey: '<', code: 'Comma' },
+  { key: '.', shiftKey: '>', code: 'Period' },
+  { key: '/', shiftKey: '?', code: 'Slash' },
+  { key: '▲', shiftKey: '▲', code: 'ArrowUp' },
+  { key: 'Shift', shiftKey: 'Shift', code: 'ShiftRight' },
+  { key: 'Ctrl', shiftKey: 'Ctrl', code: 'ControlLeft' },
+  { key: 'Win', shiftKey: 'Win', code: 'MetaLeft' },
+  { key: 'Alt', shiftKey: 'Alt', code: 'AltLeft' },
+  { key: 'Space', shiftKey: 'Space', code: 'Space' },
+  { key: 'Alt', shiftKey: 'Alt', code: 'AltRight' },
+  { key: '◄', shiftKey: '◄', code: 'ArrowLeft' },
+  { key: '▼', shiftKey: '▼', code: 'ArrowDown' },
+  { key: '►', shiftKey: '►', code: 'ArrowRight' },
+  { key: 'Ctrl', shiftKey: 'Ctrl', code: 'ControlRight' },
+];
+
+const keyboardKeysRu = [
+  { key: 'ё', shiftKey: 'Ё', code: 'Backquote' },
+  { key: '1', shiftKey: '!', code: 'Digit1' },
+  { key: '2', shiftKey: '"', code: 'Digit2' },
+  { key: '3', shiftKey: '№', code: 'Digit3' },
+  { key: '4', shiftKey: ';', code: 'Digit4' },
+  { key: '5', shiftKey: '%', code: 'Digit5' },
+  { key: '6', shiftKey: ':', code: 'Digit6' },
+  { key: '7', shiftKey: '?', code: 'Digit7' },
+  { key: '8', shiftKey: '*', code: 'Digit8' },
+  { key: '9', shiftKey: '(', code: 'Digit9' },
+  { key: '0', shiftKey: ')', code: 'Digit0' },
+  { key: '-', shiftKey: '_', code: 'Minus' },
+  { key: '=', shiftKey: '+', code: 'Equal' },
+  { key: 'Backspace', shiftKey: 'Backspace', code: 'Backspace' },
+  { key: 'Tab', shiftKey: 'Tab', code: 'Tab' },
+  { key: 'й', shiftKey: 'Й', code: 'KeyQ' },
+  { key: 'ц', shiftKey: 'Ц', code: 'KeyW' },
+  { key: 'у', shiftKey: 'У', code: 'KeyE' },
+  { key: 'к', shiftKey: 'К', code: 'KeyR' },
+  { key: 'е', shiftKey: 'Е', code: 'KeyT' },
+  { key: 'н', shiftKey: 'Н', code: 'KeyY' },
+  { key: 'г', shiftKey: 'Г', code: 'KeyU' },
+  { key: 'ш', shiftKey: 'Ш', code: 'KeyI' },
+  { key: 'щ', shiftKey: 'Щ', code: 'KeyO' },
+  { key: 'з', shiftKey: 'З', code: 'KeyP' },
+  { key: 'х', shiftKey: 'Х', code: 'BracketLeft' },
+  { key: 'ъ', shiftKey: 'Ъ', code: 'BracketRight' },
+  { key: '\\', shiftKey: '/', code: 'Backslash' },
+  { key: 'Del', shiftKey: 'Del', code: 'Delete' },
+  { key: 'Caps Lock', shiftKey: 'Caps Lock', code: 'CapsLock' },
+  { key: 'ф', shiftKey: 'Ф', code: 'KeyA' },
+  { key: 'ы', shiftKey: 'Ы', code: 'KeyS' },
+  { key: 'в', shiftKey: 'В', code: 'KeyD' },
+  { key: 'а', shiftKey: 'А', code: 'KeyF' },
+  { key: 'п', shiftKey: 'П', code: 'KeyG' },
+  { key: 'р', shiftKey: 'Р', code: 'KeyH' },
+  { key: 'о', shiftKey: 'О', code: 'KeyJ' },
+  { key: 'л', shiftKey: 'Л', code: 'KeyK' },
+  { key: 'д', shiftKey: 'Д', code: 'KeyL' },
+  { key: 'ж', shiftKey: 'Ж', code: 'Semicolon' },
+  { key: 'э', shiftKey: 'Э', code: 'Quote' },
+  { key: 'Enter', shiftKey: 'Enter', code: 'Enter' },
+  { key: 'Shift', shiftKey: 'Shift', code: 'ShiftLeft' },
+  { key: 'я', shiftKey: 'Я', code: 'KeyZ' },
+  { key: 'ч', shiftKey: 'Ч', code: 'KeyX' },
+  { key: 'с', shiftKey: 'С', code: 'KeyC' },
+  { key: 'м', shiftKey: 'М', code: 'KeyV' },
+  { key: 'и', shiftKey: 'И', code: 'KeyB' },
+  { key: 'т', shiftKey: 'Т', code: 'KeyN' },
+  { key: 'ь', shiftKey: 'Ь', code: 'KeyM' },
+  { key: 'б', shiftKey: 'Б', code: 'Comma' },
+  { key: 'ю', shiftKey: 'Ю', code: 'Period' },
+  { key: '.', shiftKey: ',', code: 'Slash' },
+  { key: '▲', shiftKey: '▲', code: 'ArrowUp' },
+  { key: 'Shift', shiftKey: 'Shift', code: 'ShiftRight' },
+  { key: 'Ctrl', shiftKey: 'Ctrl', code: 'ControlLeft' },
+  { key: 'Win', shiftKey: 'Win', code: 'MetaLeft' },
+  { key: 'Alt', shiftKey: 'Alt', code: 'AltLeft' },
+  { key: 'Space', shiftKey: 'Space', code: 'Space' },
+  { key: 'Alt', shiftKey: 'Alt', code: 'AltRight' },
+  { key: '◄', shiftKey: '◄', code: 'ArrowLeft' },
+  { key: '▼', shiftKey: '▼', code: 'ArrowDown' },
+  { key: '►', shiftKey: '►', code: 'ArrowRight' },
+  { key: 'Ctrl', shiftKey: 'Ctrl', code: 'ControlRight' },
+];
+
+class Button {
+  constructor({
+                keyEn,
+                shiftKeyEn,
+                keyRu,
+                shiftKeyRu,
+                code,
+              }) {
+    this.keyEn = keyEn;
+    this.shiftKeyEn = shiftKeyEn;
+    this.keyRu = keyRu;
+    this.shiftKeyRu = shiftKeyRu;
+    this.code = code;
+  }
+
+  // Создаем генератор кнопок
+  generateButton() {
+    let templateEn = '';
+    let templateRu = '';
+    const button = document.createElement('div');
+    button.className = 'keyboard-key';
+    button.classList.add(`${this.code}`);
+
+    const spanEn = document.createElement('div');
+    spanEn.className = 'En';
+    const spanRu = document.createElement('div');
+    spanRu.className = 'Ru';
+
+    if (this.keyEn) {
+      (templateEn += `<span class='down-key key'>${this.keyEn}</span>`);
+    }
+    if (this.shiftKeyEn) {
+      (templateEn += `<span class='shift-key key hidden'>${this.shiftKeyEn}</span>`);
+    }
+
+    if (this.keyRu) {
+      (templateRu += `<span class='down-key key'>${this.keyRu}</span>`);
+    }
+    if (this.shiftKeyRu) {
+      (templateRu += `<span class='shift-key key hidden'>${this.shiftKeyRu}</span>`);
+    }
+
+    spanEn.innerHTML = templateEn;
+    spanRu.innerHTML = templateRu;
+
+    // добавляем элементы spanEn и spanRu внутрь элемента button
+    button.appendChild(spanEn);
+    button.appendChild(spanRu);
+
+    return button;
+  }
+}
 
 // Создаем функцию соединения значений двух раскладок
 const keyboardKeysEnRu = keyboardKeysEn.map((enKey, index) => {
